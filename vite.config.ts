0241e89286db11ce1aea01b77fc80c7151d7ac19
Michel Vermeer <mvermeer85@gmail.com>
2024-05-30 20:17:48 +0200

    v.1.0.6 - Clean package

--- vite.config.ts
+++ vite.config.ts
@@ -3,36 +3,46 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import dts from "vite-plugin-dts";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), dts()],
-  server: {
-    port: 1337,
-  },
-  build: {
-    //Specifies that the output of the build will be a library.
-    lib: {
-      //Defines the entry point for the library build. It resolves
-      //to src/index.ts,indicating that the library starts from this file.
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "ftr-timetable",
-      //A function that generates the output file
-      //name for different formats during the build
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+export default defineConfig(({ mode }) => {
+  if (mode === "lib") {
+    return {
+      plugins: [
+        react(),
+        dts({
+          rollupTypes: true,
+          include: ["src/timetable/*", "src/index.ts"],
+          exclude: ["**/*.spec.*"],
+        }),
+      ],
+      server: {
+        port: 1337,
       },
+      build: {
+        outDir: "package/dist",
+        lib: {
+          entry: path.resolve(__dirname, "src/index.ts"),
+          name: "ftr-timetable",
+          fileName: (format) => `index.${format}.js`,
+        },
+        rollupOptions: {
+          external: ["react", "react-dom"],
+          output: {
+            globals: {
+              react: "React",
+              "react-dom": "ReactDOM",
+            },
+          },
+        },
+        sourcemap: true,
+        emptyOutDir: true,
+      },
+    };
+  }
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 1337,
     },
-    //Generates sourcemaps for the built files,
-    //aiding in debugging.
-    sourcemap: true,
-    //Clears the output directory before building.
-    emptyOutDir: true,
-  },
+  };
 });
